efc3c72a4692ac457a2384dfb8f6b04b661ad1ea
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-02-25 10:26:52 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    pass
+
+# ask for users credetials
+# call for authentication & receive token
+# call for repositories retrieval (using token) and receive data
+# return data
+
+if __name__ = "__main__":
+    main()
